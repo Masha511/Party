e1cdbfe07360d0c7d0693c9ac3009b9fc64d6be4
Masha <zlatkovic.masa@outlook.com>
2019-02-08 20:11:53 +0100

    home

---
+++ PartyOrganizer/HomeVC/HomeVC.swift
@@ -0,0 +1,18 @@
+//
+//  HomeVC.swift
+//  PartyOrganizer
+//
+//  Created by Masha on 2/8/19.
+//  Copyright © 2019 Maša Zlatković. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
