f2c83e7950af6a0271a8c131dc10faff009634ec
Masha <zlatkovic.masa@outlook.com>
2019-02-08 19:47:02 +0100

    Initial Commit

---
+++ PartyOrganizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PartyOrganizer
+//
+//  Created by Ilija Antonijevic on 2/8/19.
+//  Copyright © 2019 Maša Zlatković. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
