62be4fbfe7bf69ba8d648ec0d26dfccd2524fa6d
Masha <zlatkovic.masa@outlook.com>
2019-02-11 11:20:07 +0100

    search

--- PartyOrganizer/ViewControllers/Parties/PartiesVC/PartiesVC.swift
+++ PartyOrganizer/ViewControllers/Parties/PartiesVC/PartiesVC.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
-class PartiesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
+class PartiesVC: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate
 {
     @IBOutlet weak var partiesTableView: UITableView!
     @IBOutlet weak var createBtn: UIButton!
-
+    
+    private var resultIndexes: [Int]
+    {
+        if self.searchedText == nil || searchedText!.isEmpty
+        {
+            var indexes = [Int]()
+            indexes += 0..<User.shared.parties.count
+            return indexes
+        }
+        else
+        {
+            return User.shared.parties.enumerated().compactMap({ (pair) -> Int? in
+                if pair.element.name.lowercased().contains(searchedText!.lowercased())
+                {
+                    return pair.offset
+                }
+                return nil
+            })
+        }
+    }
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -20,6 +40,14 @@ class PartiesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
         
         let backBarButton = UIBarButtonItem(title: "Home", style: .plain, target: nil, action: nil)
         self.navigationItem.backBarButtonItem = backBarButton
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    deinit
+    {
+        NotificationCenter.default.removeObserver(self)
     }
     
     override func viewDidAppear(_ animated: Bool)
@@ -33,11 +61,16 @@ class PartiesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
         if User.shared.parties.count == 0
         {
             partiesTableView.isHidden = true
+            self.navigationItem.leftBarButtonItem = nil
+            self.hideSearchBar()
         }
         else
         {
             partiesTableView.isHidden = false
             self.partiesTableView.reloadData()
+            let searchItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(showSearchBar(_:)))
+            searchItem.tag = 0
+            self.navigationItem.leftBarButtonItem = searchItem
         }
     }
     
@@ -47,26 +80,43 @@ class PartiesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
         {
             if let sender = sender as? PartyCell
             {
-                partyEditorVC.present(forParty: self.partiesTableView.indexPath(for: sender)?.row)
+                if let cellIndex = self.partiesTableView.indexPath(for: sender)?.row
+                {
+                    partyEditorVC.present(forParty: resultIndexes[cellIndex])
+                }
             }
             else
             {
                 partyEditorVC.present(forParty: nil)
             }
-            
         }
+    }
+    
+    //MARK: -Keyboard Frame
+    @objc func keyboardWillShow(_ notification: Notification)
+    {
+        if let keyboardFrame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+        {
+            
+            self.partiesTableView.contentInset.bottom = keyboardFrame.height
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification)
+    {
+        self.partiesTableView.contentInset.bottom = 0.0
     }
     
     //MARK: -Table View Data Source & Delegate
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return User.shared.parties.count
+        return resultIndexes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PartyCell") as! PartyCell
-        cell.set(party: User.shared.parties[indexPath.row])
+        cell.set(party: User.shared.parties[resultIndexes[indexPath.row]])
         return cell
     }
     
@@ -84,10 +134,76 @@ class PartiesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
     {
         if editingStyle == .delete
         {
-            User.shared.parties.remove(at: indexPath.row)
+            User.shared.parties.remove(at: resultIndexes[indexPath.row])
             tableView.deleteRows(at: [indexPath], with: .fade)
             refreshView()
         }
+    }
+    
+    //MARK: -Search
+    @IBOutlet weak var seacrhHeight_c: NSLayoutConstraint!
+    @objc func showSearchBar(_ sender: Any)
+    {
+        if self.navigationItem.titleView == nil
+        {
+            showSearchBar()
+        }
+    }
+    
+    private var searchedText: String?
+    {
+        didSet
+        {
+            self.partiesTableView.reloadData()
+        }
+    }
+    
+    private func showSearchBar()
+    {
+        searchedText = nil
+        hideNavigationButtons()
+        
+        let searchBar = UISearchBar()
+        searchBar.delegate = self
+        searchBar.showsCancelButton = true
+        self.navigationItem.titleView = searchBar
+        
+        searchBar.becomeFirstResponder()
+
+    }
+    
+    private func hideNavigationButtons()
+    {
+        self.navigationItem.leftBarButtonItem = nil
+        self.navigationItem.rightBarButtonItem = nil
+    }
+    
+    @objc private func hideSearchBar()
+    {
+        searchedText = nil
+        self.navigationItem.titleView = nil
+        showNavigationButtons()
+    }
+    
+    private func showNavigationButtons()
+    {
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addParty(_:)))
+        refreshView()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String)
+    {
+        self.searchedText = searchText
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar)
+    {
+        hideSearchBar()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar)
+    {
+        searchBar.resignFirstResponder()
     }
     
     //MARK: -Actions
